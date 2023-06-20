d70814b2b012a2b485d33627d2c4be41629cc47d
vedanta28 <20cs02001@iitbbs.ac.in>
2023-06-20 13:10:25 +0530

    Update getMRP function

--- scraper/rawData.py
+++ scraper/rawData.py
@@ -1,12 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
 
-# Get your own user agent at:
-# https://explore.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes#parse-useragent
-user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
-headers = {'User-Agent': user_agent, 'Accept-Language': 'en-US, en;q=0.5'}
-url = 'https://www.amazon.in/WONDERLAND-FOODS-DEVICE-Raisins-Kishmish/dp/B08VGDWJRM?ref_=Oct_DLandingS_D_07408eea_0'
-
 def get_title(soup):
     try:
         title = soup.find("span", attrs={"id":'productTitle'}).text.strip()
@@ -15,13 +9,16 @@ def get_title(soup):
     return title
 def get_MRP(soup):
     try:
-        MRP = price = soup.find("span", attrs={"class":'a-price a-text-price a-size-base'}).span.text
+        MRP = soup.find("span", attrs={"class":'a-price a-text-price a-size-base'}).span.text
     except AttributeError:
-        MRP = ""
+        try:
+            MRP = soup.find("span", attrs={"class":'a-price a-text-price'}).span.text
+        except AttributeError:
+            MRP = ""
     return MRP
 def get_rating(soup):
     try:
-        rating = rating_section.find("span", attrs={"class":'a-size-base a-color-base'}).text
+        rating = rating_section.find("span", attrs={"class":'a-size-base a-color-base'}).text.strip()
     except AttributeError:
         rating = ""
     return rating
@@ -40,19 +37,25 @@ def get_rating_cnt(soup):
 
 
 
+if __name__ == '__main__':
+    # Get your own user agent at:
+    # https://explore.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes#parse-useragent
+    user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+    headers = {'User-Agent': user_agent, 'Accept-Language': 'en-US, en;q=0.5'}
 
+    url = 'https://www.amazon.in/WONDERLAND-FOODS-DEVICE-Raisins-Kishmish/dp/B08VGDWJRM?ref_=Oct_DLandingS_D_07408eea_0&th=1'
 
-html_text = requests.get(url, headers=headers).text
-soup = BeautifulSoup(html_text, 'lxml')
-centreCol = soup.find("div", attrs={"class":'centerColAlign'})
-title = get_title(soup=centreCol)
-MRP = get_MRP(soup=centreCol)
+    html_text = requests.get(url, headers=headers).text
+    soup = BeautifulSoup(html_text, 'lxml')
+    centreCol = soup.find("div", attrs={"class":'centerColAlign'})
+    title = get_title(soup=centreCol)
+    MRP = get_MRP(soup=centreCol)
 
-rating_section = soup.find("div", attrs={"id":'averageCustomerReviews'})
-rating = get_rating(rating_section)
-rating_cnt = get_rating_cnt(rating_section)
+    rating_section = soup.find("div", attrs={"id":'averageCustomerReviews'})
+    rating = get_rating(rating_section)
+    rating_cnt = get_rating_cnt(rating_section)
 
-print(f'Title: {title}')
-print(f'MRP: {MRP}')
-print(f'Rating: {rating}')
-print(f'Rating Count: {rating_cnt}')
+    print(f'Title: {title}')
+    print(f'MRP: {MRP}')
+    print(f'Rating: {rating}')
+    print(f'Rating Count: {rating_cnt}')
